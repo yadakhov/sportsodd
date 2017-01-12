cea9936c643466711f906c2f7b4af20d5493507e
Yada Khov <yada.khov@gmail.com>
2017-01-12 05:21:25 -0800

    Add decimal to fraction chart.

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -6,8 +6,23 @@ class MainTest extends TestCase
 {
     public function testHarness()
     {
+        echo "American | Decimal\n";
+        echo "-------- | -------\n";
+
         for ($i = -2000; $i <= 1400; $i = $i + 5) {
             echo "$i | " . SportsOdd::americanToDecimal($i) . PHP_EOL;
+        }
+
+        echo '-----------------------------------------------------------' . PHP_EOL;
+
+        echo "Decimal | American\n";
+        echo "------- | --------\n";
+
+        for ($i = 1.01; $i <= 2; $i = $i + 0.01) {
+            echo "$i | " . SportsOdd::decimalToAmerican($i) . PHP_EOL;
+        }
+        for ($i = 2; $i <= 15; $i++) {
+            echo "$i | " . SportsOdd::decimalToAmerican($i) . PHP_EOL;
         }
     }
 }
